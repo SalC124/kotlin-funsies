0825b86015562ff9f21f9d627ae0a7ee9c23a09c
SalC124 <saltcal124@gmail.com>
2024-10-14 17:27:47 -0400

    learning a bit about variables

--- src/variablesReference.kt
+++ src/variablesReference.kt
@@ -6,9 +6,25 @@ fun main() {
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
 
+    // name = "you can't change a val"
+
+    println("Hello, $name!")
+
+
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+        println("i + 1 = ${i + 1}")
     }
+    var money: Float = 1000.0F
+
+    println("money = \$$money")
+
+    var dollarBills: Int? = 1000
+
+    println("dollar bills = \$$dollarBills")
+
+    dollarBills = null
+
+    println("null safety with Int? (it isnt by default) -> $dollarBills")
 }
